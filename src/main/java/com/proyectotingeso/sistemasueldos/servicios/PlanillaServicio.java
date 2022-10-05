a54d062b4d0607f004c34d86d84b48cfc29ac56e
Alen Galindo <alen.galindo@usach.cl>
2022-10-04 23:26:07 -0300

    preubas de planila

--- src/main/java/com/proyectotingeso/sistemasueldos/servicios/PlanillaServicio.java
+++ src/main/java/com/proyectotingeso/sistemasueldos/servicios/PlanillaServicio.java
@@ -66,64 +66,16 @@ public class PlanillaServicio {
             System.out.println(empleadoEntidad.getRut());
 
             PlanillaEntidad nuevaPlanilla = new PlanillaEntidad();
-            String fecha_contratacion = empleadoEntidad.getFecha_contratacion();
-            nuevaPlanilla.setMonto_bono((int) (calculosControlador.getBonosAnnosServicio(fecha_contratacion)));
             nuevaPlanilla.setNombre(empleadoEntidad.getNombre());
             nuevaPlanilla.setRut(empleadoEntidad.getRut());
+            nuevaPlanilla.setSalud(0);
+            nuevaPlanilla.setMonto_bono(0);
+            nuevaPlanilla.setLiquido(0);
+            nuevaPlanilla.setPrevisional(0);
             nuevaPlanilla.setCategoria(empleadoEntidad.getCategoria());
             nuevaPlanilla.setSueldo_fijo(empleadoEntidad.getSueldo_fijo());
-            nuevaPlanilla.setMonto_bono(0);
-            nuevaPlanilla.setMonto_extras(0);
+            nuevaPlanilla.setAnnos_servicio(10);
             nuevaPlanilla.setMonto_descuentos(0);
-
-            //Calculo de horas extra y descuentos por atraso
-            for (AsistenciaEntidad asistencia : (ArrayList<AsistenciaEntidad>) asistenciaServicio.getAsistencias()) {
-                for (AutorizacionEntidad autorizacion : (ArrayList<AutorizacionEntidad>) autorizacionServicio.getAutorizaciones()) {
-                    String rut_empleado = empleadoEntidad.getRut();
-                    String rut_asistencia = asistencia.getRut();
-                    String fecha_asistencia = asistencia.getFecha();
-                    String fecha_autorizacion = autorizacion.getFecha();
-                    String rut_autorizacion = autorizacion.getRut();
-                    int sueldo_bruto = empleadoEntidad.getSueldo_fijo();
-                    int hora_llegada = convertidor.getHora(asistencia.getHora());
-                    int hora_minutos_llegada = convertidor.getMinutos(asistencia.getHora());
-                    int hora_salida = convertidor.getHora(asistencia.getHora());
-                    int monto_hora_extra = 0;
-                    int inasistencia = 0;
-                    float descuento = 0;
-
-                    if (rut_empleado.equals(rut_autorizacion) && rut_empleado.equals(rut_asistencia) && fecha_asistencia.equals(fecha_autorizacion) && hora_salida > 18) {
-                        monto_hora_extra = calculosControlador.montoHorasExtra(hora_salida, empleadoEntidad.getCategoria());
-                        nuevaPlanilla.setMonto_extras(nuevaPlanilla.getMonto_extras() + monto_hora_extra);
-                    }
-
-                    if (hora_llegada == 8 && hora_minutos_llegada > 0) {
-                        if (justificativoServicio.getByFecha(fecha_asistencia) == false) {
-                            int hora = convertidor.getHora(asistencia.getHora());
-                            int minutos = convertidor.getMinutos(asistencia.getHora());
-                            float des;
-                            if ((des = calculosControlador.getDescuentoAtraso(hora, minutos)) < 0) {
-                                inasistencia = inasistencia + 1;
-                            } else {
-                                descuento = descuento + des;
-                            }
-                        }
-                    }
-                }
-            }
-
-            float sueldo = calculosControlador.sueldoNoCot(nuevaPlanilla.getMonto_extras(), nuevaPlanilla.getMonto_bono(), nuevaPlanilla.getMonto_descuentos());
-
-            float previsional = sueldo * 0.1f;
-            float salud = sueldo * 0.08f;
-
-            nuevaPlanilla.setPrevisional(previsional);
-            nuevaPlanilla.setSalud(salud);
-
-            float liquido = sueldo - (salud + previsional);
-            nuevaPlanilla.setLiquido(liquido);
-
-            System.out.println(nuevaPlanilla.getNombre());
             savePlanilla(nuevaPlanilla);
         }
     }
